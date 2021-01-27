68e15a31c74765da01878931b3593ee2f77fd02f
JuliaDer <julia.dernovska@gmail.com>
2021-01-27 13:03:08 +0100

    Created data

---
+++ Intermediate/Day 17: Quiz Game/data.py
@@ -0,0 +1,68 @@
+question_data = [
+    {
+        "category": "Geography",
+        "question": "Greenland is almost as big as Africa.",
+        "answer": "False"
+    },
+    {
+        "category": "Geography",
+        "question": "A group of islands is called an archipelago.",
+        "answer": "True"
+    },
+    {
+        "category": "Geography",
+        "question": "Greenland is covered with grass and Iceland covered with ice.",
+        "answer": "False"
+    },
+    {
+        "category": "Geography",
+        "question": "Vatican City is a country.",
+        "answer": "True"
+    },
+    {
+        "category": "Geography",
+        "question": "San Marino is the only country completely surrounded by another country.",
+        "answer": "False"
+    },
+    {
+        "category": "Geography",
+        "question": "There is a city called Rome in every continent on Earth.",
+        "answer": "False"
+    },
+    {
+        "category": "Geography",
+        "question": "Ottawa is the capital of Canada.",
+        "answer": "True"
+    },
+    {
+        "category": "Geography",
+        "question": "California is larger than Japan.",
+        "answer": "True"
+     },
+    {
+        "category": "Geography",
+        "question": "Tokyo is the capital of Japan.",
+        "answer": "True"
+    },
+    {
+        "category": "Geography",
+        "question": "Hungary is the only country in the world beginning with H.",
+        "answer": "False"
+    },
+    {
+        "category": "Geography",
+        "question": "New Haven is the capital city of the state of Connecticut in the United States.",
+        "answer": "False"
+    },
+    {
+        "category": "Geography",
+        "question": "Nova Scotia is on the east coast of Canada.",
+        "answer": "True"
+    },
+    {
+        "category": "Geography", "type": "boolean", "difficulty": "easy",
+        "question": "There is an island in Japan called kunoshima, "
+                    "A.K.A. 'Rabbit Island', so named because of it's huge population of rabbits.",
+        "answer": "True"
+    }
+]
