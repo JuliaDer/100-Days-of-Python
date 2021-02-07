666c88996fd6738add68ac2313ae199e5380bc76
JuliaDer <julia.dernovska@gmail.com>
2021-02-07 19:05:06 +0100

    Create scoreboard

---
+++ Intermediate/Day 20: Snake Game/scoreboard.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+STYLE = ('Times', 20, 'bold')
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.penup()
+        self.color("white")
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align="center", font=STYLE)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center", font=STYLE)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
