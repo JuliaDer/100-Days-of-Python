4c4c850c1872e61985a809933085f99cf7c8375d
JuliaDer <julia.dernovska@gmail.com>
2021-02-09 09:44:33 +0100

    Main file

---
+++ Intermediate/Day 22: Pong Game/main.py
@@ -0,0 +1,23 @@
+from turtle import Screen
+from paddle import Paddle
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong Game")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+screen.listen()
+screen.onkey(r_paddle.up, "Up")
+screen.onkey(r_paddle.down, "Down")
+screen.onkey(l_paddle.up, "w")
+screen.onkey(l_paddle.down, "s")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
+screen.exitonclick()
