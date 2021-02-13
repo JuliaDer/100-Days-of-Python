35eee69db2a40d6e702a897be7e152a73c288c58
JuliaDer <julia.dernovska@gmail.com>
2021-02-13 20:24:46 +0100

    Save missing states in new *csv file

--- Intermediate/Day 25: CSV Data and Pandas/U.S. States Game/main.py
+++ Intermediate/Day 25: CSV Data and Pandas/U.S. States Game/main.py
@@ -14,6 +14,14 @@ guessed_states = []
 while len(guessed_states) < 50:
     answer_state = screen.textinput(f"{len(guessed_states)}/50 States Correct", "What's another state's name?").title()
 
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
     if answer_state in all_states:
         guessed_states.append(answer_state)
         t = turtle.Turtle()
@@ -22,5 +30,3 @@ while len(guessed_states) < 50:
         state_data = data[data.state == answer_state]
         t.goto(int(state_data.x), int(state_data.y))
         t.write(answer_state)
-
-screen.exitonclick()
