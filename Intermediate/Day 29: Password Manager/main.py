15f23ef968223c22603851d03c06e04b17d863a0
JuliaDer <julia.dernovska@gmail.com>
2021-03-03 13:39:08 +0100

    Handling Exceptions in the Password Manager

--- Intermediate/Day 29: Password Manager/main.py
+++ Intermediate/Day 29: Password Manager/main.py
@@ -40,16 +40,21 @@ def save():
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
-        with open("data.json", "r") as data_file:
-            # Reading old data
-            data = json.load(data_file)
+        try:
+            with open("data.json", "r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
             # Updating old data with new data
             data.update(new_data)
 
-        with open("data.json", "w") as data_file:
-            # Saving updated data
-            json.dump(new_data, data_file, indent=4)
-
+            with open("data.json", "w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
             website_input.delete(0, END)
             password_input.delete(0, END)
 
